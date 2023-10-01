f5660167660baecfb0979de0238c655975b61082
Rudransh Kaushik <99244245+Rudranshkaushik@users.noreply.github.com>
2023-10-01 18:18:31 +0530

    Add files via upload

---
+++ face_blur.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+
+import cv2
+import mediapipe as mp
+
+
+def process_img(img, face_detection):
+
+    H, W, _ = img.shape
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    out = face_detection.process(img_rgb)
+
+    if out.detections is not None:
+        for detection in out.detections:
+            location_data = detection.location_data
+            bbox = location_data.relative_bounding_box
+
+            x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+
+            x1 = int(x1 * W)
+            y1 = int(y1 * H)
+            w = int(w * W)
+            h = int(h * H)
+
+            # print(x1, y1, w, h)
+
+            # blur faces
+            img[y1:y1 + h, x1:x1 + w, :] = cv2.blur(img[y1:y1 + h, x1:x1 + w, :], (30, 30))
+
+    return img
+
+
+args = argparse.ArgumentParser()
+
+args.add_argument("--mode", default='webcam')
+# args.add_argument("--filePath", default=None)
+
+args = args.parse_args()
+
+
+output_dir = './output'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# detect faces
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
+
+    if args.mode in ["image"]:
+        # read image
+        # img = cv2.imread(args.filePath)
+        path = 'C:\\Users\\rudra\\Pictures\\Camera Roll\\WIN_20230817_15_09_27_Pro.jpg'
+        img = cv2.imread(path)
+
+
+
+        img = process_img(img, face_detection)
+
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
+
+    elif args.mode in ['webcam']:
+        cap = cv2.VideoCapture(0)
+
+        ret, frame = cap.read()
+        while ret:
+            frame = process_img(frame, face_detection)
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            ret, frame = cap.read()
+
+        cap.release()
